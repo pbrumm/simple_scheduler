eeb15f585f56a410dd7e727fe949206c26180dbf
Pete Brumm <pete@petebrumm.com>
2022-05-26 16:34:26 -0500

    adding a job that only the path is needed

---
+++ simple_scheduler/jobs/curl_path_job.py
@@ -0,0 +1,43 @@
+"""A job to send a HTTP (GET or DELETE) periodically."""
+
+import logging
+import requests
+import io
+
+from ndscheduler.corescheduler import job
+
+logger = logging.getLogger(__name__)
+
+
+class CurlPathJob(job.JobBase):
+
+    @classmethod
+    def meta_info(cls):
+        url_name = os.environ.get("CURL_PATH_URL") || "http://localhost/api/v1/"
+        return {
+            "job_class_string": "%s.%s" % (cls.__module__, cls.__name__),
+            "notes": "This sends a HTTP GET request to %s" % (url_name),
+            "arguments": [
+                # path
+                {"type": "string", "description": "what path do we add to this request", },
+                
+            ],
+            "example_arguments": (
+                '["jobs"]' 
+            ),
+        }
+
+    def run(self, path, *args, **kwargs):
+        url_name = os.environ.get("CURL_PATH_URL") || "http://localhost/api/v1/"
+        url = "%s%s" % (url_name, path)
+        print("Start GET on url: %s" % (url))
+
+        session = requests.Session()
+        result = session.request(request_type, url, headers=None, data=None)
+        print("Calling GET on url: %s" % (url))
+        return result.text
+
+
+if __name__ == "__main__":
+    job = CurlPathJob.create_test_instance()
+    job.run("http://localhost:888/api/v1/jobs")
